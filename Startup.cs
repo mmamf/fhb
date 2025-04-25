75ccc0b7820c47305c8c8567403d9aba24edb1d4
mmamf <106527714+mmamf@users.noreply.github.com>
2025-04-24 23:07:35 -0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartupAttribute(typeof(SMS.Startup))]
+namespace SMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
